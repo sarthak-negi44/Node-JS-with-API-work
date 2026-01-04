876c5baa177469f78dec3dedce373c760d5fefb1
Sarthak Negi <sarthakn658@gmail.com>
2026-01-04 15:48:40 +0530

    node JS joke - generator

---
+++ joke-generator.js
@@ -0,0 +1,23 @@
+import https from "https";
+import chalk from "chalk";
+const getJoke = ()=>{
+    const url = 'https://official-joke-api.appspot.com/random_joke';
+    https.get(url,(response) =>{
+        let data = "";
+        response.on('data',(chunk) =>{
+            data += chunk
+        });
+        response.on('end',() =>{
+            const joke = JSON.parse(data);
+          console.log(joke);
+            // console.log(`here a random ${joke.type} joke`);
+            // console.log(`chalk.red (${joke.setup})`);
+            // console.log(`chalk.blue.bgRed.bold (${joke.punchline})`);
+
+        });
+        response.on('err',() =>{
+          console.log(`error fetching the joke, ${err.message}`);
+        })
+    })
+}
+getJoke();
